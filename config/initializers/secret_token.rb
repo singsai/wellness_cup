3a05c754a5ef48890ebb9446934f6d528f69225c
singsai <nick.tang@gmail.com>
2013-01-25 12:37:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WellnessCup::Application.config.secret_token = 'f4fa263bf7dc47e7ef57a9a55fb13dce5a65c442e6f880751c5a7f8a0726ab9915076987b20c3b61840965c48a180dcb8d23034cb08c1a31126508f167f91311'
